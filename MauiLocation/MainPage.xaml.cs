f23c05c24b1fca08acdf91b9cfcb2605bb27f81d
KALYAN <allam.kalyana@outlook.com>
2024-04-12 17:52:01 +0530

    Add project files.

---
+++ MauiLocation/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiLocation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiLocation/Platforms/Android/MainApplication.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace MauiLocation
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        [assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+        [assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+        [assembly: UsesFeature("android.hardware.location", Required = false)]
+        [assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+        [assembly: UsesFeature("android.hardware.location.network", Required = false)]
+        [assembly: UsesPermission(Manifest.Permission.AccessBackgroundLocation)]
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
